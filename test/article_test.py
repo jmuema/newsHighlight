c03a62dcca2a7ccfa06b90931d2f557a85109d19
josephmuema <jmuema95@gmail.com>
2019-05-17 16:23:26 +0300

    Fixed an error in source test

---
+++ test/article_test.py
@@ -0,0 +1,16 @@
+import unittest
+from app.models import Articles
+
+class ArticleTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the source class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_article = Articles("cnbc",'CNBC','Stocks',"https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/10/24/104791816-GettyImages-856703910.1910x1000.jpg","Check out the companies","2019","a date")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_article,Articles))
---
+++ test/source_test.py
@@ -0,0 +1,17 @@
+import unittest
+from  app.models import Sources
+
+
+class SourceTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the source class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_source = Sources("foxnews",'Fox News','Joaqin Guzman is dangerous',"https://www.foxnews.com/us/el-chapo-accused-of-drugging-rapping-girls-as-young-as-13-in-court-documents")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,Sources))
